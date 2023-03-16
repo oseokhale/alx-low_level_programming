b81a6550b0d36157e65a814e3aae3928b1d3e5b3
oseokhalee <oseokhalee@gmail.com>
2023-03-16 07:08:37 -0700

    Task 05

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C programm that prints a line using pprintf function
+ * Return: 0 (NoError);
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
