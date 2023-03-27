a2ff3016004bbfbd615b944170a9a292fdc48573
oseokhalee <oseokhalee@gmail.com>
2023-03-27 09:45:42 -0700

    Task 03

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout followed by a new line
+ * @str: the string to be printed
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
