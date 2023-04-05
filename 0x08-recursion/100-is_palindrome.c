ad0ab8354076511c3fc80632c807b60eb242b11b
oseokhalee <oseokhalee@gmail.com>
2023-04-05 12:34:33 -0700

    Task 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -40,4 +40,23 @@ int check_palindrome(char *s, int len, int index)
 	if (s[index] == s[len - index - 1])
 		return (check_palindrome(s, len, index + 1));
 
+	return (0);
+}
 
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
+int is_palindrome(char *s)
+{
+	int index = 0;
+	int len = find_strlen(s);
+
+	if (!(*s))
+		return (1);
+
+	return (check_palindrome(s, len, index));
+}
