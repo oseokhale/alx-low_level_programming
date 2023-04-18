dd78c0942a4a6d9aef4d3e5faeab20c20039ed0e
oseokhalee <oseokhalee@gmail.com>
2023-04-18 01:02:32 -0700

    Task 00

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A new type description for dog
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ * Description: Define a new type struct dog with the following elements
+ */
+
+struct dog
+{
+
